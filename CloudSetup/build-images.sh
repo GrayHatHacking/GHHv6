539db973d582ec67dda57977910ffddfc422970d
sussurro <sussurro@happypacket.net>
2021-01-30 19:15:14 -0500

    fixed to include explicit path of packer

--- CloudSetup/build-images.sh
+++ CloudSetup/build-images.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd kali-base
-packer build kali.json
+/usr/bin/packer build kali.json
 cd ../server-2016-base
-packer build server2016_template.json
+/usr/bin/packer build server2016_template.json
 cd ../server-2019-base
-packer build server2019_template.json
+/usr/bin/packer build server2019_template.json
 cd ..
