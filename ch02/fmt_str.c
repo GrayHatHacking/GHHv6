9b7f7742464dd093afafe4d718b7ff762dbf4caa
Kali <harperaa@gmail.com>
2021-01-31 21:01:09 -0500

    first commit of GHHv6/ch02

---
+++ ch02/fmt_str.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void){
+  double x = 23.5644;
+  printf("The value of x is %5.2f\n", x);
+  printf("The value of x is %4.1f\n", x);
+
+  return 0;
+}
---
+++ ch02/hello.c
@@ -0,0 +1,6 @@
+// hello.c                   // customary comment of program name
+#include <stdio.h>           // needed for screen printing
+int main(){                  // required main function
+  printf("Hello haxor!\n");  // simply say hello
+}                            // exit program
+
