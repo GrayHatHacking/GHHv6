60b12188efdd6492eb925c103c3f0938d545a3be
sussurro <sussurro@happypacket.net>
2021-02-14 02:58:40 -0500

    add the build instruciton

--- CloudSetup/build-images.sh
+++ CloudSetup/build-images.sh
@@ -6,7 +6,7 @@ fi
 cd kali-base
 $PACKER build kali.json
 cd ../server-2016-base
-$PACKER server2016_template.json
+$PACKER build server2016_template.json
 cd ../server-2019-base
-$PACKER server2019_template.json
+$PACKER build server2019_template.json
 cd ..
