4c7770266cc932fc0413815112b322016706d57b
mosesrenegade <moses@moses.io>
2021-02-18 21:14:24 -0500

    In Kali I did not have python3-pip, just a note that I modified the install-cloud-tools.sh. Please note that I added some output logging as well.

--- CloudSetup/install-cloud-tools.sh
+++ CloudSetup/install-cloud-tools.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+
+#Kali VMWare Image doesn't have pip3
+
+echo "[+] Installing Python3 pip"
+sudo apt install python3-pip -y
+
+echo "[+] Installing Ansible and pywinrm"
 pip3 install --user ansible pywinrm
+
+echo "[+] Installing Terraform 0.14.5"
 wget https://releases.hashicorp.com/terraform/0.14.5/terraform_0.14.5_linux_amd64.zip
+
+echo "[+] Installing Packer"
 wget https://releases.hashicorp.com/packer/1.6.6/packer_1.6.6_linux_amd64.zip
 unzip terraform*.zip -d ~/.local/bin/
 unzip packer*.zip -d ~/.local/bin/
