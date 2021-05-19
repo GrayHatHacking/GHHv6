17380ad36b4df42b597e3bf71d824e11e0cf313a
dan <daniel.kuehr@tacitosecurity.com>
2021-05-19 16:45:22 -0300

    small fixes

--- ch25/labs/lab7/guest.py
+++ ch25/labs/lab7/guest.py
@@ -7,7 +7,7 @@ class Guest:
 
     def __enter__(self):
         self.proc = Popen(
-            'qemu-system-x86_64 -display none -boot d -cdrom kernel_bios.iso -m 300M -serial stdio -enable-kvm',
+            'exec qemu-system-x86_64 -display none -boot d -cdrom kernel_bios.iso -m 300M -serial stdio -enable-kvm',
             stdout=PIPE, stdin=PIPE, shell=True
             )
         return self
