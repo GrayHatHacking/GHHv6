8fc40580e7eabec8c21f99a2c58d5b0907fc71cf
mosesrenegade <moses@moses.io>
2021-07-06 10:09:37 -0400

    Remove SSM

--- ch28/lab/terraform/ssm.tf
+++
@@ -1,7 +0,0 @@
-#resource "aws_ssm_document" "perform_healthcheck" {
-#  name            = "PerformHealthcheck"
-#  document_type   = "Command"
-#  document_format = "YAML"
-
-#  content = file("documents/perform_healthcheck.yml")
-#}
