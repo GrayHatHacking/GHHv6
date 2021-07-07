a9e31cb5cc9e45a52483c6517d9597ce70ef0106
mosesrenegade <moses@moses.io>
2021-07-06 22:49:17 -0400

    Fixed for TE

--- ch31/Lab/terraform/main.tf
+++ ch31/Lab/terraform/main.tf
@@ -4,8 +4,8 @@ provider "aws" {
   profile                 = "ghh"
 }
 
-#provider "random_pet" {}
-
-resource "random_pet" "s3" {
-  length = 2
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
 }
