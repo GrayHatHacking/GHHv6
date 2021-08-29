af316ddbeaa982d35cdf78c7a3e6963827208942
sussurro <sussurro@happypacket.net>
2021-08-29 00:08:43 -0400

    fixed typos and made better

--- ch17/Run-SDProp.ps1
+++ ch17/Run-SDProp.ps1
@@ -1,5 +1,9 @@
 $dom = Get-ADDomain
 $RootDSE = New-Object System.DirectoryServices.DirectoryEntry("LDAP://$($dom.PDCEmulator)/RootDSE")
 $RootDSE.UsePropertyCache = $false
-$RootDSE.Put($TaskName, "1") # RunProtectAdminGroupsTask & fixupinheritance
+$RootDSE.Put("RunProtectAdminGroupsTask", "1") 
+$RootDSE.SetInfo()
+$RootDSE = New-Object System.DirectoryServices.DirectoryEntry("LDAP://$($dom.PDCEmulator)/RootDSE")
+$RootDSE.UsePropertyCache = $false
+$RootDSE.Put("fixupinheritance","1")
 $RootDSE.SetInfo()
