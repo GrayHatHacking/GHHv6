471528e913843da393cdb3ec04eecd1c1ed9cd43
sussurro <sussurro@happypacket.net>
2021-10-02 22:37:38 -0400

    updated lab to remove util

--- ch07/Lab/terraform/outputs.tf
+++ ch07/Lab/terraform/outputs.tf
@@ -1,10 +1,8 @@
 output "dc_password" {     value = rsadecrypt(aws_instance.ghh_dc.password_data, file(var.key_path)) }
 output "target_password" {     value = rsadecrypt(aws_instance.ghh_target.password_data, file(var.key_path)) }
-output "util_password" {     value = rsadecrypt(aws_instance.ghh_util.password_data, file(var.key_path)) }
 
 output "dc_ip" { value =  try(aws_instance.ghh_dc.public_ip,null) }
 output "target_ip" { value =  try(aws_instance.ghh_target.public_ip,null) }
-output "util_ip" { value =  try(aws_instance.ghh_util.public_ip,null) }
 output "kali_ip" { value =  try(aws_instance.kali.public_ip,null) }
 
 
@@ -43,23 +41,6 @@ resource "local_file" "target_inv" {
  }
  )
  filename = "../ansible/inventory/target.yml"
-}
-resource "local_file" "util_inv" {
- content = templatefile("./inventory.tmpl",
- {
-  cat = "util",
-  ip_addrs = [aws_instance.ghh_util.public_ip]
-  vars = ["ansible_connection: winrm",
-	"ansible_user: Administrator",
-	"ansible_winrm_scheme: http",
-        "ansible_winrm_transport: basic",
-        "ansible_winrm_server_cert_validation: ignore",
-        "ansible_winrm_port: 5985",
-        "ansible_password: ${var.dc_password}"
-        ]
- }
- )
- filename = "../ansible/inventory/util.yml"
 }
 resource "local_file" "kali_inv" {
  content = templatefile("./inventory.tmpl",
